207a12cc9b98d0f41bd34de65cd4f23028a26820
Lucas <md.lucasd@gmail.com>
2024-01-31 15:22:50 -0300

    teste codewall

--- src/helpers/getLineHeight.ts
+++ src/helpers/getLineHeight.ts
@@ -7,6 +7,7 @@ export const getLineHeight = (props: any): number => {
   const lineHeight: Flatten<LineHeight> = props.lineHeightVariant;
   const fontSizeFromTheme = getTheme(`fontSizes.${fontSize}`)(props);
   const lineHeightVariant = getTheme(`lineHeight.${lineHeight}`)(props);
+  console.log('teste codewall');
   const lineHeightPercentage = lineHeightVariant
     ? toOnlyNumbers(lineHeightVariant as string)
     : '120';
